f73e1271648214c4154d90656f0a5455ea2eb87e
maxovic <maxat.tileuov@gmail.com>
2019-02-01 14:28:11 +0600

    final variant

---
+++ Week 1/Task 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // Reading a line by converting it to INT using the function "Parse". n = length of array.
+            string[] numb = Console.ReadLine().Split(' '); /* Next step, We need to read our N integers as string,
+            then, the function Split helps us to seperate our digits.
+            */
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(numb[i]); // Numb's elements are still in String, thus I'm converting it into int.
+                int ok = 1;
+                for (int j = 2; j <= x / 2; j++) // Identifying the primes
+                {
+                    if (x % j == 0)
+                    {
+                        ok = 0;
+                        break;
+                    }
+                }
+                if (x > 1 && ok == 1)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        list.Add(x);
+                        Console.Write(x + " ");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Week 1/Task 4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); // As we can't directly insert integers, firstly, we need to read it as string;
+            int n = int.Parse(s); // Function "Parse" allows us to convert string to int;
+            string[,] arr = new string[n, n]; // Created 2D array;
+            /* 
+             00 01 02 03
+             10 11 12 13
+             20 21 22 23
+             30 31 32 33
+                
+             Increase 'j' till j <= i and to those positions put "[*]";
+             */
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    arr[i, j] = "[*]";
+                }
+            }
+
+            // Showing elements in [i,j<=i]
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            // Easier way to do that
+            /* 
+             for (int i = 1; i <= n; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("[*]");
+                }
+                Console.WriteLine();
+            }
+              */
+
+        }
+    }
+}
