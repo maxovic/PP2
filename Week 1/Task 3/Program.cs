78f5f4ec14a57861ab33cf32751107fa9f8f8deb
maxovic <maxat.tileuov@gmail.com>
2019-02-09 00:58:04 +0600

    refresh

--- Week 1/Task 3/Program.cs
+++ Week 1/Task 3/Program.cs
@@ -7,36 +7,37 @@ using System.Threading.Tasks;
 namespace Task_3
 {
     class Program
-    {
-        static void Main(string[] args)
+    {   
+        static void method(string a, string b)
         {
-            int n = int.Parse(Console.ReadLine()); // Reading a line by converting it to INT using the function "Parse". n = length of array.
-            string[] numb = Console.ReadLine().Split(' '); /* Next step, We need to read our N integers as string,
-            then, the function Split helps us to seperate our digits.
-            */
-            List<int> list = new List<int>();
-            for (int i = 0; i < n; i++)
+            int n = int.Parse(a); // int.Parsing(Converting to int) the given string 'a' to int;
+            string[] arr = b.Split(' '); // Separating the string 'b';
+            int[] array = new int[n]; // new array;
+            for(int i = 0; i < n; i++)
             {
-                int x = int.Parse(numb[i]); // Numb's elements are still in String, thus I'm converting it into int.
-                int ok = 1;
-                for (int j = 2; j <= x / 2; j++) // Identifying the primes
+                int x = int.Parse(arr[i]);
+                array[i] = x; // inserting digits from string[] arr to int[] array;
+
+            }
+            List<int> list = new List<int>();
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < 2; j++)
                 {
-                    if (x % j == 0)
-                    {
-                        ok = 0;
-                        break;
-                    }
-                }
-                if (x > 1 && ok == 1)
-                {
-                    for (int k = 0; k < 2; k++)
-                    {
-                        list.Add(x);
-                        Console.Write(x + " ");
-                    }
+                    list.Add(array[i]); // adding every element of int[] array twice in list
                 }
             }
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+        }
 
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string s = Console.ReadLine();
+            method(n, s);
         }
     }
 }
